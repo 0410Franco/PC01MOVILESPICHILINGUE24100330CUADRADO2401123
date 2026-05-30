bddd2eef4fe2bf832616df145618b9960e2e53e0
Jean <24101123@ue.edu.pe>
2026-05-30 10:30:49 -0500

    primer commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 
     //Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.9.8")
---
+++ app/src/main/java/com/example/uesanapp/presentation/Travel/LocationPermissionScreen.kt
@@ -0,0 +1,9 @@
+package com.example.uesanapp.presentation.travel
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LocationPermissionScreen() {
+    Text("Permisos de Ubicación")
+}

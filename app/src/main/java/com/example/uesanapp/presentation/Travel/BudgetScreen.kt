b02312f1e84ce55c8fd6c477fd735ceecaebc464
Jean <24101123@ue.edu.pe>
2026-05-30 10:40:04 -0500

    cambio

--- app/src/main/java/com/example/uesanapp/presentation/Travel/BudgetScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.uesanapp.presentation.travel
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun BudgetScreen() {
-    Text("Planificador de Presupuesto")
-}
